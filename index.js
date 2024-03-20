d3c366a59bb369b487bc949cd07bd0d0958d5fbe
br364 <134791002+vitor-gblop@users.noreply.github.com>
2024-03-19 21:05:45 -0500

    adicionando alterações finais a biblioteca

---
+++ index.js
@@ -0,0 +1,49 @@
+
+// function myFunction()
+// {
+//     if (x == 1)
+//     {
+//         document.getElementById("section").style.backgroundColor = "#3f3f3f";
+//         c = 2
+//     }
+//     else
+//     {
+//         document.getElementById("section").style.backgroundColor = "#faebd7";
+//         c = 1;
+//     }
+    
+// }
+
+let c;
+
+function roxo()
+{
+    c = 1;
+
+    if (c == 1) 
+    {   
+        document.getElementById("header").style.backgroundColor = "#8b11c0";
+        document.getElementById("menuB").style.backgroundColor = "#64099c";
+        
+    } 
+}
+function amarelo()
+{
+    c = 2;
+
+    if (c == 2) 
+    {
+        document.getElementById("header").style.backgroundColor = "#e0ca47";
+        document.getElementById("menuB").style.backgroundColor = "#b8a61c";
+    } 
+}
+function verde()
+{
+    c = 3;
+
+    if (c == 3) 
+    {
+        document.getElementById("header").style.backgroundColor = "#64de9f";
+        document.getElementById("menuB").style.backgroundColor = "#32b97d";
+    } 
+}
